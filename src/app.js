bcd922ed7a381cfc3a27698e2070e7c8af643906
unknown <xlingao@gmail.com>
2020-04-06 20:39:30 +1000

    Set up app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const getweather = require("./getweather");
 // const request = require("request");
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 const publicDirPath = path.join(__dirname, "../public");
 const viewPath = path.join(__dirname, "../templates/views");
 const partialsPath = path.join(__dirname, "../templates/partials");
@@ -101,6 +103,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000...");
+app.listen(port, () => {
+  console.log("Server is up on port " + port + "...");
 });
